fbf63005bed9e5a71e90ba0656b0d5f2cabf3f35
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-04-24 23:14:47 -0400

    correctly handle multipart form data

--- gliva.scm
+++ gliva.scm
@@ -5,6 +5,7 @@
           data
           intarweb
           matchable
+          miscmacros
           multipart-form-data
           pages
           scheme
@@ -12,8 +13,11 @@
           uri-common
           utf8)
 
+  ; --------------------------------------------------------------------------
+
   (define (authorized-for-request?)
-    (let ((authorization (header-value 'authorization (request-headers (current-request)))))
+    (let ((authorization (header-value 'authorization
+                                       (request-headers (current-request)))))
       (and (vector? authorization)
            (eqv? (vector-ref authorization 0) 'basic)
            (string=? (cdr (assoc 'username (vector-ref authorization 1)))
@@ -27,13 +31,38 @@
         (send-response status: 'unauthorized
                        headers: `((www-authenticate . (#(basic ((realm . "gliva")))))))))
 
+  ; --------------------------------------------------------------------------
+
+  (define (save-multipart-file multipart-file)
+    ; FIXME: so many things wrong with this
+    (let ((filename (multipart-file-filename multipart-file)))
+      (begin
+        (with-output-to-file (string-join (root-path) "/" filename)
+                             (lambda ()
+                               (copy-port (multipart-file-port file)
+                                          (current-output-port))))
+        #f)))
+
+  (define (handle-multipart-form-data)
+    (let ((form-data (read-multipart-form-data (current-request))))
+      (filter-map (lambda (form-input)
+             (if (multipart-file? (cdr form-input))
+                 (save-multipart-file (cdr form-input))
+                 form-input)))))
+
+  ; --------------------------------------------------------------------------
+
   (define (handle-home-page)
     (send-response status: 'ok
                    body: (render-home-page (select-info) (select-works))))
 
   (define (handle-work-page slug)
     (send-response status: 'ok
-                   body: (render-work-page (select-info) (select-work slug) (select-works))))
+                   body: (render-work-page (select-info)
+                                           (select-work slug)
+                                           (select-works))))
+
+  ; --------------------------------------------------------------------------
 
   ; TODO edge cases for all admin pages!
 
@@ -47,7 +76,7 @@
                                body: (render-admin-info-page (select-info))))
         ('(post) (begin
                   ; Special case: info is a singleton
-                  (update-info (read-urlencoded-request-data request) 1)
+                  (update-info (handle-multipart-form-data) 1)
                   (send-response status: 'ok
                                  body: (render-admin-info-page (select-info)))))
         (else (send-response status: 'method-not-allowed)))))
@@ -58,7 +87,7 @@
         ('(get) (send-response status: 'ok
                                body: (render-admin-works-page (select-works))))
         ('(post) (begin
-                   (insert-work (read-urlencoded-request-data request))
+                   (insert-work (handle-multipart-form-data))
                    (send-response status: 'ok
                                   body: (render-admin-works-page (select-works)))))
         (else (send-response status: 'method-not-allowed)))))
@@ -69,22 +98,17 @@
         ('(get) (send-response status: 'ok
                                body: (render-admin-work-page (select-work id))))
         ('(post) (begin
-                  (update-work (read-urlencoded-request-data request) id)
+                  (update-work (handle-multipart-form-data) id)
                   (send-response status: 'ok
                                  body: (render-admin-work-page (select-work id)))))
         (else (send-response status: 'method-not-allowed)))))
 
-  (define (handle-upload-file)
-    (let ((file (read-multipart-form-data (current-request))))
-      (begin
-        (with-output-to-file (multipart-file-filename file)  ; FIXME: so wrong
-                             (lambda ()
-                               (copy-port (multipart-file-port file)
-                                          (current-output-port))))
-        (send-response status: 'ok))))
+  ; --------------------------------------------------------------------------
 
   (define (handle-404-page)
     (send-response status: 'not-found body: "Life is complicated. Or is it?"))
+
+  ; --------------------------------------------------------------------------
 
   (define (route-admin-request admin-path)
     (match admin-path
@@ -92,7 +116,6 @@
       (("info") (handle-admin-info-page))
       (("works") (handle-admin-works-page))
       (("work" id) (handle-admin-work-page id))
-      (("upload") (handle-upload-file))
       (_ (handle-404-page))))
 
   (define (route-request continue)
@@ -104,6 +127,7 @@
           (('/ "favicon.ico") (send-static-file "favicon.ico"))
           (('/ "static" filename) (send-static-file filename))
           (('/ "work" slug) (handle-work-page slug))
-          (('/ "admin" tail ...) (call-with-authorization route-admin-request tail))
+          (('/ "admin" tail ...) (call-with-authorization route-admin-request
+                                                          tail))
           (_ (handle-404-page)))
         (continue)))))
